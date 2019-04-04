dafbb48fd2230751778bacb3c5a41bae311af120
Gennady Portenko <gportenko@gmail.com>
2019-04-04 16:06:38 +0400

    Added Lombok @Data annotation to HibernateExample

--- HibernateExample/src/main/java/webstudio/WebStudio.java
+++ HibernateExample/src/main/java/webstudio/WebStudio.java
@@ -1,46 +1,16 @@
 package webstudio;
 
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class WebStudio {
-    private int id;
-    private String name;
-    private double annualProfit;
-    private int employeesNum;
-
-    public WebStudio(String name, double annualProfit, int employeesNum) {
-        this.name = name;
-        this.annualProfit = annualProfit;
-        this.employeesNum = employeesNum;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public double getAnnualProfit() {
-        return annualProfit;
-    }
-
-    public int getEmployeesNum() {
-        return employeesNum;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setAnnualProfit(double annualProfit) {
-        this.annualProfit = annualProfit;
-    }
-
-    public void setEmployeesNum(int employeesNum) {
-        this.employeesNum = employeesNum;
-    }
+    int id;
+    @NonNull
+    String name;
+    @NonNull
+    double annualProfit;
+    @NonNull
+    int employeesNum;
 }

3962a4fd1355f65db34218e35962fee2d10952f4
Gennady Portenko <gportenko@gmail.com>
2019-04-05 13:53:24 +0400

    HibernateExample : Added Annotation configuration instead of XML configuration.

--- HibernateExample/src/main/java/hibernateexample/HibernateExample.java
+++ HibernateExample/src/main/java/hibernateexample/HibernateExample.java
@@ -4,16 +4,34 @@ import org.hibernate.HibernateException;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.cfg.Configuration;
 import webstudio.WebStudio;
 
+import java.util.Properties;
+
 public class HibernateExample {
-    private static SessionFactory factory;
+    private static SessionFactory factory = null;
+    private static StandardServiceRegistry registry = null;
 
     public static void main(String... args)  {
 
         try {
-            factory = new Configuration().configure().buildSessionFactory();
+           // XML configuration :
+           // factory = new Configuration().configure().buildSessionFactory();
+
+           // Java-based configuration :
+           Properties prop = new Properties();
+                prop.setProperty("connection.driver_class", "com.mysql.jdbc.Driver");
+                prop.setProperty("hibernate.connection.url", "jdbc:mysql://localhost/test");
+                prop.setProperty("hibernate.connection.username", "test");
+                prop.setProperty("hibernate.connection.password", "test");
+                prop.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQLDialect");
+            factory =  new Configuration()
+                    .addAnnotatedClass(WebStudio.class)
+                    .setProperties(prop)
+                    .buildSessionFactory();
+
         } catch (Throwable ex) {
             System.err.println("Failed to create sessionFactory object." + ex);
             throw new ExceptionInInitializerError(ex);
@@ -21,9 +39,9 @@ public class HibernateExample {
 
         HibernateExample he = new HibernateExample();
 
-        Integer id01 = he.addWebStudio("ProStudio", 5450030.5d, 10);
+        Integer addedStudioId = he.addWebStudio("ProStudio", 5450030.5d, 10);
 
-        System.out.println(id01);
+        System.out.println(addedStudioId);
     }
 
     public Integer addWebStudio(String name, double annualProfit, int employeesNum){
--- HibernateExample/src/main/java/webstudio/WebStudio.java
+++ HibernateExample/src/main/java/webstudio/WebStudio.java
@@ -3,14 +3,28 @@ package webstudio;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
+import javax.persistence.*;
+
 @Data
+@Entity
+@Table(name = "WEBSTUDIO")
 @FieldDefaults(level = AccessLevel.PRIVATE)
 public class WebStudio {
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name = "id")
     int id;
+
     @NonNull
+    @Column(name="name")
     String name;
+
     @NonNull
+    @Column(name="annual_profit")
     double annualProfit;
+
     @NonNull
+    @Column(name="employees_num")
     int employeesNum;
 }
+

0dd508a796039438558eec3548e14e72f9078c51
Gennady Portenko <gportenko@gmail.com>
2019-04-11 11:01:15 +0400

    SpringCoreContextExample :   added try-with-resources for auto closing of AnnotationConfigurationApplicationContext

--- SpringCoreContextExample/src/main/java/gpk/practice/spring/corecontext/app/annotationconfigapp/AnnotationConfigurationApp.java
+++ SpringCoreContextExample/src/main/java/gpk/practice/spring/corecontext/app/annotationconfigapp/AnnotationConfigurationApp.java
@@ -12,20 +12,22 @@ import org.springframework.context.annotation.Configuration;
                  "gpk.practice.spring.corecontext.app.annotationconfigapp.connection"})
 public class AnnotationConfigurationApp implements IApp {
     public void start() {
-        ApplicationContext context = new AnnotationConfigApplicationContext(AnnotationConfigurationApp.class);
         /*
         ApplicationContext context = new AnnotationConfigApplicationContext(
                 "gpk.practice.spring.corecontext.app.annotationconfigapp.dao",
                 "gpk.practice.spring.corecontext.app.annotationconfigapp.connection");
         */
+        try (AnnotationConfigApplicationContext context =
+                new AnnotationConfigApplicationContext(AnnotationConfigurationApp.class)) {
 
-        SomeDAO dao1 = context.getBean(SomeDAO.class);
-        SomeDAO dao2 = context.getBean(SomeDAO.class);
+            SomeDAO dao1 = context.getBean(SomeDAO.class);
+            SomeDAO dao2 = context.getBean(SomeDAO.class);
 
-        System.out.println("dao1 : " );
-        dao1.printConnections();
+            System.out.println("dao1 : ");
+            dao1.printConnections();
 
-        System.out.println("dao2 : " );
-        dao2.printConnections();
+            System.out.println("dao2 : ");
+            dao2.printConnections();
+        }
     }
 }

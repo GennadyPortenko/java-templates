e2c15bd28996afc759fbe44f0a111cae0174403b
Gennady Portenko <gportenko@gmail.com>
2019-04-11 10:48:25 +0400

    SpringCoreContextExample :   added another kind of context creation for Annotation Configuration example

--- SpringCoreContextExample/src/main/java/gpk/practice/spring/corecontext/app/annotationconfigapp/AnnotationConfigurationApp.java
+++ SpringCoreContextExample/src/main/java/gpk/practice/spring/corecontext/app/annotationconfigapp/AnnotationConfigurationApp.java
@@ -4,12 +4,20 @@ import gpk.practice.spring.corecontext.app.IApp;
 import gpk.practice.spring.corecontext.app.annotationconfigapp.dao.SomeDAO;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan({"gpk.practice.spring.corecontext.app.annotationconfigapp.dao",
+                 "gpk.practice.spring.corecontext.app.annotationconfigapp.connection"})
 public class AnnotationConfigurationApp implements IApp {
     public void start() {
+        ApplicationContext context = new AnnotationConfigApplicationContext(AnnotationConfigurationApp.class);
+        /*
         ApplicationContext context = new AnnotationConfigApplicationContext(
                 "gpk.practice.spring.corecontext.app.annotationconfigapp.dao",
                 "gpk.practice.spring.corecontext.app.annotationconfigapp.connection");
+        */
 
         SomeDAO dao1 = context.getBean(SomeDAO.class);
         SomeDAO dao2 = context.getBean(SomeDAO.class);

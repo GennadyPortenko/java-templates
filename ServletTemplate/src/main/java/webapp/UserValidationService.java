a7899f4aed82066b70d1b390a61bb5e8a68b5b48
Gennady Portenko <gportenko@gmail.com>
2019-05-29 12:22:04 +0400

    Moved ServletTemplate to a serapate project (SpringMVCMavenExample, see git history)

--- ServletTemplate/src/main/java/webapp/UserValidationService.java
+++
@@ -1,7 +0,0 @@
-package webapp;
-
-public class UserValidationService {
-    public boolean validate(String login, String password) {
-        return login.equals("Gena") && password.equals("password");
-    }
-}
